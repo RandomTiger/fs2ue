2bc53195bccdc66426684918db26211f05d7778f
RandomTiger <RandomSnowTiger@gmail.com>
2017-05-23 16:24:58 +0100

    Move source

---
+++ fs2ue/Source/Test.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "fs2ue.h"
+#include "Test.h"
+
+Test::Test()
+{
+}
+
+Test::~Test()
+{
+}
---
+++ fs2ue/Source/fs2ue/fs2ue.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "fs2ue.h"
+#include "../Test.h"
+#include "../Test.cpp"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, fs2ue, "fs2ue" );
+
+Test test;
