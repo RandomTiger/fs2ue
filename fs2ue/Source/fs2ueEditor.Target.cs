44a61ad7a4b08fdf45781292e3836c14879a0c91
RandomTiger <RandomSnowTiger@gmail.com>
2017-06-20 05:47:51 +0100

    Copyright notice first pass

--- fs2ue/Source/fs2ueEditor.Target.cs
+++ fs2ue/Source/fs2ueEditor.Target.cs
@@ -1,4 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*
+* Copyright (C) Thomas Liam Whittaker 2017.  All rights reserved.
+*
+* All modified source code herein is the property of Thomas Liam Whittaker. You may not sell
+* or otherwise commercially exploit the source or things you created based on the
+* source. Original source code is owned by Volition, Inc and covered by their copywrite.
+*
+*/
 
 using UnrealBuildTool;
 using System.Collections.Generic;
